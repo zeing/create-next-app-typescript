643a0dc4a6ab8b77b8282c7e7953e196e7daa086
Zeing <zeing@outlook.com>
2025-03-06 17:33:48 +0700

    Refactor Tailwind CSS configuration and update dependencies for compatibility with Tailwind CSS v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     'postcss-nested': {},
     'postcss-import': {},
-    'postcss-preset-env': {},
-    autoprefixer: {},
   },
 }
 
